a0e3dacf6b951bbe4802519061b68c09f2b49343
Arashdeep Kaur <official.arashdeep96@gmail.com>
2022-02-22 12:55:07 -0500

    add firebase

---
+++ OnToDo/signInViewController.swift
@@ -0,0 +1,27 @@
+//
+//  signInViewController.swift
+//  OnToDo
+//
+//  Created by user206151 on 2/22/22.
+//  Copyright © 2022 user206151. All rights reserved.
+//
+import UIKit
+class signInViewController: UIViewController {
+
+    @IBOutlet weak var fName: UITextField!
+    @IBOutlet weak var lName: UITextField!
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func signUpButton(_ sender: Any) {
+    }
+    @IBAction func signInButton(_ sender: Any) {
+    }
+    
+}
