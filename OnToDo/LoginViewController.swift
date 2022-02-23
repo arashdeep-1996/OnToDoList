29561bd410c6fe418b27d471731935484476bee6
sruthy jacob <sruthylisa@gmail.com>
2022-02-23 11:29:52 -0500

    modified login vc

--- OnToDo/LoginViewController.swift
+++ OnToDo/LoginViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
+
+class LoginViewController: UIViewController{
 
     @IBOutlet weak var textFieldEmail: UITextField!
     
@@ -18,21 +20,57 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        textFieldEmail.delegate = self
-        textFieldPassword.delegate = self
+        
     }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.textFieldEmail.resignFirstResponder()
-        self.textFieldPassword.resignFirstResponder()
-        return true
-    }
+    override func viewDidAppear(_ animated: Bool) {
+            checkUserInfo()  // check if already a user is logged in
+        }
+        
 
     @IBAction func signInBtn(_ sender: Any) {
+        validateFields()
     }
     
     
     @IBAction func signUpBtn(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "signUp")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
     }
     
-}
+    
+    func validateFields() {
+            if textFieldEmail.text?.isEmpty == true {
+                print("Please enter your email")
+                return
+            }
+            
+            if textFieldPassword.text?.isEmpty == true {
+                print("Please enter your password")
+                return
+            }
+            
+            login()
+        }
+        
+        func login() {
+            Auth.auth().signIn(withEmail: textFieldEmail.text!, password: textFieldPassword.text!) { [weak self] authResult, err in
+                guard let strongSelf = self else {return}
+                if let err = err {
+                    print(err.localizedDescription)
+                }
+                self.checkUserInfo()
+            }
+        
+        }
+        
+        func checkUserInfo() {
+            if Auth.auth().currentUser != nil {
+                print(Auth.auth().currentUser?.uid)
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "Home")
+                vc.modalPresentationStyle = .overFullScreen
+                present(vc, animated: true)        }
+        }
+        }
