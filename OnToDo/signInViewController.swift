ad605de51e2a305028c0c2b07b37aed24d0bf33b
Arashdeep Kaur <official.arashdeep96@gmail.com>
2022-02-22 21:00:56 -0500

    add changes in ui

--- OnToDo/signInViewController.swift
+++ OnToDo/signInViewController.swift
@@ -6,20 +6,46 @@
 //  Copyright © 2022 user206151. All rights reserved.
 //
 import UIKit
+import FirebaseAuth
 class signInViewController: UIViewController {
 
     @IBOutlet weak var fName: UITextField!
     @IBOutlet weak var lName: UITextField!
     @IBOutlet weak var Email: UITextField!
     @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var SignUP: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        Email.placeholder = "E-Mail"
+        Email.layer.borderWidth = 1
+        Email.layer.borderColor = UIColor.black.cgColor
+        fName.placeholder = "First Name"
+        fName.layer.borderWidth = 1
+        fName.layer.borderColor = UIColor.black.cgColor
+        lName.placeholder = "Last Name"
+        lName.layer.borderWidth = 1
+        lName.layer.borderColor = UIColor.black.cgColor
+        Password.placeholder = "Password"
+        Password.layer.borderWidth = 1
+        Password.layer.borderColor = UIColor.black.cgColor
+        SignUP.layer.cornerRadius = 30
+       
     }
 
     @IBAction func signUpButton(_ sender: Any) {
+        guard let email = Email.text, !email.isEmpty,
+            let password = Password.text, !password.isEmpty else{
+                print("Missing field data")
+                return
+        }
+        //Get auth instance
+        //attempt sign in
+        //if failure, present alert to create account
+        //if user continues, create account
+        
+        //check sign in on app launch
+        //allow user to sign out with button
     }
     @IBAction func signInButton(_ sender: Any) {
     }
